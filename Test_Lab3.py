f66695a2a1dc84c052f13d7a81f36b8327455297
Chan Mya Mya Thein <chanmmthein03.23@ichat.sp.edu.sg>
2024-10-31 15:41:17 +0800

    Updated Test File

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -19,7 +19,7 @@ def test_bubble_sort_descending():
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
-    assert (result == test_arr)
+    assert (result == test_arr) 
 
 def test_bubble_sort_invalid():
     result = []
@@ -28,3 +28,19 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+
+def test_bubble_sort_greaterThan10():
+     input_arr = [64, 34, 25, 12, 22, 11, 90, 56, 43, 23, 11]
+     result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+     assert (result == 1)
+
+def test_bubble_sort_empty():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert(result == 0)
+
+def test_bubble_sort_notInteger():
+    input_arr = [64, 34,"22", 25, 12, 22, 11, 90, 23, 45, 55, 12]
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert(result == 2)
